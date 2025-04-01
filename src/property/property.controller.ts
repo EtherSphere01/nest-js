9d68274bd869daf15db36ad725d0fdf1f8b5ed4c
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-04-01 23:05:15 +0600

    first commit

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,9 +1,31 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 
 @Controller('property')
 export class PropertyController {
   @Get()
   findAll() {
-    return 'all properties';
+    return 'all properties new';
+  }
+
+  @Get('onlyget')
+  findAll2() {
+    return 'all properties new';
+  }
+
+  @Get(':id/:action')
+  findOne(@Param('id') id: string, @Param('action') action: string) {
+    return `property ${id} with action ${action}`;
+  }
+
+  //   @Post()
+  //   create() {
+  //     return 'create property new';
+  //   }
+
+  @Post()
+  @HttpCode(202)
+  create2(@Body() body: any) {
+    return body;
   }
 }
